c14463fae977da57d8ca30ef8872a7b407d78098
Lav <lavdigikraftsocial@gmail.com>
2026-04-06 13:20:46 +0530

    commit

--- server/routes/aboutSnippetRoutes.js
+++ server/routes/aboutSnippetRoutes.js
@@ -1,36 +1,9 @@
 import express from "express";
-import AboutSnippet from "../models/AboutSnippet.js";
+import { saveAboutSnippet, getAboutSnippet } from "../controllers/aboutSnippetController.js";
 
 const router = express.Router();
 
-// @route   GET /api/about
-// @desc    Get About Snippet
-router.get("/", async (req, res) => {
-  try {
-    const snippet = await AboutSnippet.findOne();
-    if (snippet) {
-      res.status(200).json(snippet);
-    } else {
-      res.status(404).json({ message: "About snippet not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// @route   POST /api/about/save
-// @desc    Save or Update About Snippet
-router.post("/save", async (req, res) => {
-  try {
-    const snippet = await AboutSnippet.findOneAndUpdate(
-      {}, // Pehla document dhoondega ya naya banayega (upsert)
-      req.body,
-      { new: true, upsert: true, runValidators: true }
-    );
-    res.status(200).json(snippet);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+router.get("/", getAboutSnippet);
+router.post("/save", saveAboutSnippet);
 
 export default router;

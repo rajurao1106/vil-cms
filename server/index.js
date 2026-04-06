cd729eb0072327b4ef16db2dbf3fde73f1aefadd
Lav <lavdigikraftsocial@gmail.com>
2026-04-06 13:04:19 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 1337;
 
 // Only run server in local environment
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") { 
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
